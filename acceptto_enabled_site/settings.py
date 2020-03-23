26a0bd89f7c79d7d044389786a220b8a384d858e
Sinan <shmorcel@wpi.edu>
2020-03-23 05:56:24 -0400

    Partially completed the authentication backend

--- acceptto_enabled_site/settings.py
+++ acceptto_enabled_site/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Acceptto MFA Integration
+MFA_APP_UID = 99999999999999999999999
+MFA_APP_SECRET = 99999999999999999999999
+MFA_SITE = "https://mfa.acceptto.com"
--- acceptto_mfa/backends.py
+++ acceptto_mfa/backends.py
@@ -1,9 +1,49 @@
 from django.conf import settings
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth.models import User
+import requests
+import logging as log
 
 class AccepttoMFABackend(BaseBackend):
   """
   Authenticate using the Acceptto MFA API using the UID and secret defined in the settings MFA_APP_UID and MFA_APP_SECRET.
   """
-  pass
+  def authenticate(self, request, username=None):
+    app_uid = settings.MFA_APP_UID
+    app_secret = settings.MFA_APP_SECRET
+    mfa_site = settings.MFA_SITE
+
+    payload = {
+      "uid": app_uid,
+      "secret": app_secret,
+      "email": username,
+      "message": "Would you like to login into Acceptto MFA enalbed Django Sample App?",
+    }
+    response = requests.post(mfa_site + "/api/v9/authenticate_with_options", data=payload)
+    if (response.status_code == 200):  # success
+      # create a new user in Django, if the user is not there already
+      channel = response.json()["channel"]  # pass it somehow to the view
+      try:
+        user = User.objects.get(username=username)
+      except User.DoesNotExist:
+        # create a new user
+        user = User(username=username)
+        user.is_staff = False
+        user.is_superuser = False
+        user.save()
+      return user
+    elif response.status_code == 401:  # Email address provided is not a valid registered Acceptto account!
+      return None
+    elif response.status_code == 403:  # Invalid uid and secret combination, Application not found!
+      # create a new user in Django, if the user is not there already
+      log.error("The Application's UID or Secret were incorrect. Make sure that you have set them correctly in the site's settings.py file, or that you have created an application in the eGuardian® dashboard.")
+      raise RuntimeError()  # just crash... for now
+    else:  # other errors (shouldn't happen)
+      log.error("An unexpected error occured.")
+      raise RuntimeError() 
+
+  def get_user(self, user_id):
+    try:
+      return User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+      return None
