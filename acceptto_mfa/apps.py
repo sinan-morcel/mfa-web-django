36bdc9fb90dffd548d583703093f92208d0b29a7
Sinan <shmorcel@wpi.edu>
2020-03-24 05:57:38 -0400

    Now credentials can be provided as a json file

--- acceptto_mfa/apps.py
+++ acceptto_mfa/apps.py
@@ -1,16 +1,18 @@
 from django.apps import AppConfig
 from django.conf import settings
+import json
 
 # fetch credentials
-f = open(settings.BASE_DIR + '/credentials.txt', "r")
-lines = f.readlines()
+f = open(settings.BASE_DIR + '/credentials.json', "r")
+# lines = f.readlines()
+obj = json.load(f)
 f.close()
 
 class AccepttoMfaConfig(AppConfig):
     name = 'acceptto_mfa'
     # Acceptto MFA Integration
-    mfa_app_uid = lines[0].strip()
-    mfa_app_secret = lines[1].strip()
+    mfa_app_uid = obj["uid"]
+    mfa_app_secret = obj["secret"]
     mfa_site = "https://mfa.acceptto.com"
 
 # override the authentication backend when this app is installed
